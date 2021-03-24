34854747f965e19f62ae621bd090839f9ecdf6bf
XCoreX <xcorex77@gmail.com>
2021-03-24 20:46:49 +0200

    2-2-1-4

---
+++ src/com/alpha/PracticalWork23/Task23.java
@@ -0,0 +1,11 @@
+package com.alpha.PracticalWork23;
+
+import com.alpha.PracticalWork23.work1.Runner;
+
+public class Task23 {
+
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.run();
+    }
+}
