de8baf35f1e7cb7018cc8c413e0fa4206fbf7157
XCore <xcorex77@gmail.com>
2021-05-07 10:45:08 +0300

    3-4-1-1

---
+++ src/com/alpha/practicalwork34/Task34.java
@@ -0,0 +1,11 @@
+package com.alpha.practicalwork34;
+
+import com.alpha.practicalwork34.work1.Runner;
+
+public class Task34 {
+
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.run();
+    }
+}
---
+++ src/com/alpha/practicalwork34/work1/Runner.java
@@ -0,0 +1,8 @@
+package com.alpha.practicalwork34.work1;
+
+public class Runner {
+
+    public void run() {
+
+    }
+}
