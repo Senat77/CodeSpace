f4773d053042039f4c84b0008bc425d53d9a004a
XCore <xcorex77@gmail.com>
2021-05-12 13:41:56 +0300

    3-4-1-1

--- src/com/alpha/practicalwork34/work1/MyTimeBomb_2.java
+++ src/com/alpha/practicalwork34/work1/MyTimeBomb_2.java
@@ -1,12 +1,12 @@
 package com.alpha.practicalwork34.work1;
 
-public class MyTimeBomb_1 extends Thread {
+public class MyTimeBomb_2 implements Runnable {
 
     @Override
     public void run() {
         for (int i = 10; i >= 0 ; i--) {
             System.out.println(i);
         }
-        System.out.println("Boom!!!");
+        System.out.println("Boooooom!!!");
     }
 }
--- src/com/alpha/practicalwork34/work1/Runner.java
+++ src/com/alpha/practicalwork34/work1/Runner.java
@@ -3,6 +3,9 @@ package com.alpha.practicalwork34.work1;
 public class Runner {
 
     public void run() {
-
+        Thread thread1 = new MyTimeBomb_1();
+        Runnable thread2 = new MyTimeBomb_2();
+        thread1.start();
+        thread2.run();
     }
 }
