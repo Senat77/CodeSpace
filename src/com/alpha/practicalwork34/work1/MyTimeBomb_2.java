2bb02629ffbae3bf680fc5a7164e249c8a05d942
XCore <xcorex77@gmail.com>
2021-05-12 13:49:18 +0300

    3-4-1-1

--- src/com/alpha/practicalwork34/work1/MyTimeBomb_2.java
+++ src/com/alpha/practicalwork34/work1/MyTimeBomb_2.java
@@ -1,11 +1,19 @@
 package com.alpha.practicalwork34.work1;
 
+import java.util.Random;
+
 public class MyTimeBomb_2 implements Runnable {
 
     @Override
     public void run() {
+        Random random = new Random();
         for (int i = 10; i >= 0 ; i--) {
-            System.out.println(i);
+            try {
+                Thread.sleep(random.nextInt(5000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(getClass().getSimpleName() + " " + i);
         }
         System.out.println("Boooooom!!!");
     }
